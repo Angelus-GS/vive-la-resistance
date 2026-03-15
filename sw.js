16842278ed879027f31fd34d745be68d91be2568
Angelus-GS <11436462+Angelus-GS@users.noreply.github.com>
2026-03-15 03:36:08 -0400

    v3.3.1 — fix duplicate notification in foreground

--- sw.js
+++ sw.js
@@ -85,7 +85,7 @@ define(['./workbox-35c88359'], (function (workbox) { 'use strict';
     "revision": "a1329a6bb9d63f77107164bce59da7f9"
   }, {
     "url": "index.html",
-    "revision": "2babfc98d4310f3e159c7b8fbed32576"
+    "revision": "55f61bc5d916ac5ec3671692c7ef5a1c"
   }, {
     "url": "icon-512x512.png",
     "revision": "cf6fdaf634d6b14dea4551499eb86e2e"
@@ -102,10 +102,10 @@ define(['./workbox-35c88359'], (function (workbox) { 'use strict';
     "url": "404.html",
     "revision": "c9f6b505778d5f82433f33fca068913a"
   }, {
-    "url": "assets/index-RqwBjzur.js",
+    "url": "assets/index-Bqh188E8.css",
     "revision": null
   }, {
-    "url": "assets/index-Bqh188E8.css",
+    "url": "assets/index-BHExRUOF.js",
     "revision": null
   }, {
     "url": "apple-touch-icon.png",
